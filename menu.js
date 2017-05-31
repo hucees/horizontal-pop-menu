f60d247c58c5b26ae8806b1fc71725e761f6e856
Hugo C. Sanchez <hucees@gmail.com>
2017-05-30 22:47:55 -0500

    Add files via upload
    pop function works with jquery

--- menu.js
+++ menu.js
@@ -1,1 +1,24 @@
 // Animation code will go here
+$( document ).ready(function() {
+  var $toggle = 0;
+  $( "#main" ).click(function() {
+    if ($toggle==0) {
+      $( ".first" ).animate({
+        opacity:"1"
+      }, { duration: 500, queue: false });
+        // Animate class second
+      $(".second").animate({
+        opacity:"1"
+      }, { duration: 1000, queue: false });
+      $toggle = 1;
+    }else{
+      $( "li:not(#main)" ).animate({
+        opacity:"0"
+      }, 500, function() {
+        // Animation complete.
+        $toggle = 0;
+      });
+    };
+
+  });
+});
