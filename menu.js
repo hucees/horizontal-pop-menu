ef5eb1be1490865d45aec75e140aec60b58eb252
Hugo C. Sanchez <hucees@gmail.com>
2017-06-01 00:48:36 -0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -3,6 +3,18 @@ $( document ).ready(function() {
   var $toggle = 0;
   $( "#main" ).click(function() {
     if ($toggle==0) {
+      $(".b").animate({
+        top:"400px"
+      },{duration:300, queue: false });
+      $(".b").animate({
+        left:"33.33%"
+      },{duration:300, queue: false });
+      $(".c").animate({
+        top:"400px"
+      },{duration:300, queue: false });
+      $(".c").animate({
+        left:"66.66%"
+      },{duration:300, queue: false });
       $( ".first" ).animate({
         opacity:"1"
       }, { duration: 500, queue: false });
@@ -10,8 +22,10 @@ $( document ).ready(function() {
       $(".second").animate({
         opacity:"1"
       }, { duration: 1000, queue: false });
+        $('li').toggleClass('open');
       $toggle = 1;
     }else{
+      $('li').toggleClass('open');
       $( "li:not(#main)" ).animate({
         opacity:"0"
       }, 500, function() {
