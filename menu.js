ad6017f98a4140a7db983926683dcb7618cfa41a
Hugo C. Sanchez <hucees@gmail.com>
2018-04-13 11:37:04 -0600

    resuming project
    Cleaning up js and cleaning up functionality.

--- menu.js
+++ menu.js
@@ -1,38 +1,39 @@
 // Animation code will go here
 $( document ).ready(function() {
-  var $toggle = 0;
-  $( "#main" ).click(function() {
+  // Function expands the menu items.
+  function expand(){
+    // Expand Li Animate
+    $(".b, .c").animate({
+      top:"400px"
+    },{duration:1000, queue: false });
+    $( "li:not(#main)" ).animate({
+      opacity:"1"
+    }, {duration: 1000, queue: false});
+    $('li').toggleClass('open');
+    return this;
+  };
+
+  // Function collapses the menu items.
+  function collapse(){
+    // Collapse Li Animate
+      $(".b, .c").animate({
+        top:"-400pxpx"
+      },{duration:300, queue: false });
+      return this;
+  };
+
+  var $toggle = 0; //This variable will be used to tell what state the menu is in after multiple clicks.
+
+  $( "#main" ).click(function() { //Function occurs on main menu button click.
     if ($toggle==0) {
-      $(".b").animate({
-        top:"400px"
-      },{duration:300, queue: false });
-      $(".b").animate({
-        left:"33.33%"
-      },{duration:300, queue: false });
-      $(".c").animate({
-        top:"400px"
-      },{duration:300, queue: false });
-      $(".c").animate({
-        left:"66.66%"
-      },{duration:300, queue: false });
-      $( ".first" ).animate({
-        opacity:"1"
-      }, { duration: 500, queue: false });
-        // Animate class second
-      $(".second").animate({
-        opacity:"1"
-      }, { duration: 1000, queue: false });
-        $('li').toggleClass('open');
+      expand(); // Expand Li Animate
       $toggle = 1;
     }else{
       $('li').toggleClass('open');
       $( "li:not(#main)" ).animate({
         opacity:"0"
-      }, 500, function() {
-        // Animation complete.
-        $toggle = 0;
-      });
+      }, {duration: 1000, queue: false});
+      $toggle = 0;
     };
-
   });
 });
